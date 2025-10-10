59b358d4ef61f8339216f2543c5e7fcd22f5a8fe
Tanishq Singh <hello@tanishqsingh.com>
2025-10-10 11:07:04 +0530

    website: remove github login, example env

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -10,18 +10,6 @@ export default async function Auth() {
         <main className="w-full bg-transparent text-white flex-1 flex flex-col justify-center items-center">
             <div className="w-full max-w-sm">
                 <h2 className="text-3xl font-bold text-center mb-6 text-orange-500 border-b-2 border-orange-600 pb-2">Register Now</h2>
-
-                <form
-                    action={async () => {
-                        "use server";
-                        await signIn("github");
-                    }}
-                >
-                    <button className="w-full bg-orange-500 text-black font-semibold py-3 rounded-full hover:bg-orange-400 transition-colors cursor-pointer" type="submit">
-                        github
-                    </button>
-                </form>
-
                 <form
                     className="mt-4"
                     action={async () => {
